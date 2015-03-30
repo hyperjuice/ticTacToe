ecb38d89153365aa3494c1e1b37f58eefc724028
Darrell Cohn <darrellcohn@gmail.com>
2015-03-29 20:32:54 -0700

    with attaempt at refactoring in OOP

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,41 +1,47 @@
 $(function() {
-	var $move = $(".box"); 
-		playerTurn = "X";
+	var playerTurn = "X";
+	function Player(symbol) {
+		this.mark = symbol;
+	}
 
-		//set the click functionality
-		$move.click(function(event) {
-			var $switchTurn = $(event.target);  
+	function Board() {
+		this.playerX = new Player("X");
+		this.playerO = new Player("O");
+		this.$boxes = $('.box');
+		this.$reset = $('#reset');
+	}
+
+	Board.prototype.switchPlayer = function() {
+		this.boxes.click(function() {
+			var $switchTurn = $(event.target);
 			if ($switchTurn.html() === "&nbsp;") {
+				if (playerTurn === "X") {
+					$switchTurn.html("X");
+					$switchTurn.css("background-color", "purple");
+				    if (checkForWinner(playerTurn)) {
+				    	$("#header").html(playerTurn + " wins!");
+				    	alert(playerTurn + " wins!");
+				    	return location.reload();
+					}
+				    playerTurn = "O";
+				    $("#header").html("Now it's O's turn");
+				} else {
+			    	$switchTurn.html("O");
+			    	$switchTurn.css("background-color", "orange");
+			    	if (checkForWinner(playerTurn)) {
+			    		$("#header").html(playerTurn + " wins!");
+			    		alert(playerTurn + " wins!");
+			    		return location.reload();
+				    }
+			    	playerTurn = "X";
+			    	$("#header").html("Now it's X's turn");
+				}
 
-				//mark a cell as "X" or "O"
-					if (playerTurn === "X") {
-						$switchTurn.html("X");
-					    $switchTurn.css("background-color", "purple");
-					    if (checkForWinner(playerTurn)) {
-					    	$("#header").html(playerTurn + " wins!");
-					    	alert(playerTurn + " wins!");
-					    	return location.reload();
-					    } 
-					    playerTurn = "O";
-					    $("#header").html("Now it's O's turn"); 
-				    } else {
-				    	$switchTurn.html("O");
-				    	$switchTurn.css("background-color", "orange");
-				    	if (checkForWinner(playerTurn)) {
-				    		$("#header").html(playerTurn + " wins!");
-				    		alert(playerTurn + " wins!");
-				    		return location.reload();
-				    	} 
-				    	playerTurn = "X";
-				    	$("#header").html("Now it's X's turn");
-					}    
-			    
-			    //gets its input from checkBox below
-			    function checkForWinner(move) {
+				function checkForWinner(move) {
 			    	var result = false;
 			    	if (checkRow(1, 2, 3, move) || 
 			    	    checkRow(4, 5, 6, move) || 
-			    	    checkRow(7, 8, 9, move) ||   // place the winner logic from line 14 and 24 into this function
+			    	    checkRow(7, 8, 9, move) ||   
 			    	    checkRow(1, 4, 7, move) ||
 			    	    checkRow(2, 5, 8, move) || 
 			    	    checkRow(3, 6, 9, move) || 
@@ -44,9 +50,8 @@ $(function() {
 			    		result = true;
 			    	}
 			    	return result;
-			    }
+				}
 
-			    //gets its input from getBox below
 			    function checkRow(a, b, c, move) {
 			    	var result = false;
 			    	if (getBox(a) === move && getBox(b) === move && getBox(c) === move) {
@@ -55,16 +60,26 @@ $(function() {
 			    	return result;
 			    }
 
-			    //
 			    function getBox(number) {
-			    	return document.getElementById("cell" + number).innerHTML;
-			    	//return $("#cell" + number).html;
+			    	return $("#cell" + number).html();
 			    }
-		    } 
-		});
+			}
+		}
+	};
 
-	//reset button
-	$("#reset").click(function() {
-		location.reload();
-	});
-});
+	Board.prototype.init = function() {
+		var _this = this;
+		this.$boxes.click(function(evnt) {
+			_this.nextPlayer();
+		}	
+	};
+
+	Board.prototype.reset = function() {
+		this.$reset.click(function(event) {
+			location.reload();
+		}
+	};
+
+	var board = new Board();
+  	board.init();
+};
